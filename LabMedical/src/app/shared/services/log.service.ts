c7658b953d9f42cb3ab0872dafb381bb06689095
Leandro-da Silva-Michail-Krikis <leandro_silva22@estudante.sesisenai.org.br>
2023-07-19 22:25:11 -0300

    Criação do metodo get all logs

--- LabMedical/src/app/shared/services/log.service.ts
+++ LabMedical/src/app/shared/services/log.service.ts
@@ -1,21 +1,38 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 import {environment} from "../../enviroments/enviroment";
-import { LogTypeEnum } from "../enums/logtype.enum";
+import {LogTypeEnum} from "../enums/logtype.enum";
+import {UserService} from "./user.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LogService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private userService: UserService) {
+  }
 
   createLog(description: string, logType: LogTypeEnum): Observable<any> {
     return this.http.post(
-      `${ environment.URL_POST_LOG }`,
+      `${environment.URL_POST_LOG}`,
       {"description": description, "logType": logType},
       {observe: 'response'}
+    )
+  }
+
+  getAll(page: number, pageSize: number): Observable<any> {
+    let user = this.userService.getUser()
+    return this.http.get(
+      `${environment.URL_LISTAR_LOG}`,
+      {
+        observe: 'response',
+        headers :  {
+          'Authorization': `Bearer ${user.access_token}`,
+          'page': page.toString(),
+          'page-size': pageSize.toString()
+        }
+      }
     )
   }
 }
