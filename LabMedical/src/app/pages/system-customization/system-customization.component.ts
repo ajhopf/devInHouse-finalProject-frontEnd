9c7c9dac18ef38f821142fb47c8beec0afffb848
Leandro-da Silva-Michail-Krikis <leandro_silva22@estudante.sesisenai.org.br>
2023-07-22 20:55:39 -0300

    Criação do componente system-customization

---
+++ LabMedical/src/app/pages/system-customization/system-customization.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-system-customization',
+  templateUrl: './system-customization.component.html',
+  styleUrls: ['./system-customization.component.css']
+})
+export class SystemCustomizationComponent {
+
+}
