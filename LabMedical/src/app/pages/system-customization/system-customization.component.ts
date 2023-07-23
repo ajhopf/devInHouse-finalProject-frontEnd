bcfd8a84a4859ca33263c01f1702eee5242c5ae4
Leandro-da Silva-Michail-Krikis <leandro_silva22@estudante.sesisenai.org.br>
2023-07-22 23:08:41 -0300

    Ajustes para que seja buscada a configuração do servidor

--- LabMedical/src/app/pages/system-customization/system-customization.component.ts
+++ LabMedical/src/app/pages/system-customization/system-customization.component.ts
@@ -1,22 +1,21 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ConfigService} from "../../shared/services/config.service";
+import {error} from "@angular/compiler-cli/src/transformers/util";
+import {SystemConfigModel} from "../../shared/models/system-config.model";
 
 @Component({
   selector: 'app-system-customization',
   templateUrl: './system-customization.component.html',
   styleUrls: ['./system-customization.component.css']
 })
-export class SystemCustomizationComponent {
+export class SystemCustomizationComponent implements OnInit {
   systemConfigForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.systemConfigForm = this.formBuilder.group({
-      companyName: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
-      logoUrl: ['', [Validators.required, Validators.pattern('(https?|data):[^\\s/$.?#].[^\\s]*')]],
-      primaryColor: ['', [Validators.required, Validators.pattern('^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$')]],
-      secondaryColor: ['', [Validators.required, Validators.pattern('^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$')]],
-      fontColor: ['', [Validators.required, Validators.pattern('^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$')]],
-    });
+  constructor(private formBuilder: FormBuilder, private systemConfigService: ConfigService) {}
+
+  ngOnInit(): void {
+    this.inicializeForm();
   }
 
   onSubmit(): void {
@@ -35,4 +34,23 @@ export class SystemCustomizationComponent {
   checkIfInputIsUsed(inputName: string): boolean {
     return this.systemConfigForm.controls[inputName].dirty || this.systemConfigForm.controls[inputName].touched
   }
+
+  private inicializeForm() {
+    this.systemConfigService.getSystemConfig().subscribe({
+      next: (systemConfigModel: SystemConfigModel): void => {
+        this.systemConfigForm.setValue(systemConfigModel)
+      },
+      error: (err: any) => {
+        console.log(err)
+      }
+    })
+    this.systemConfigForm = this.formBuilder.group({
+      companyName: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
+      logoUrl: ['', [Validators.required, Validators.pattern('(https?|data):[^\\s/$.?#].[^\\s]*')]],
+      primaryColor: ['', [Validators.required, Validators.pattern('^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$')]],
+      secondaryColor: ['', [Validators.required, Validators.pattern('^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$')]],
+      fontColor: ['', [Validators.required, Validators.pattern('^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$')]],
+    });
+  }
+
 }
