610d33c51e63c4aefcb99d2f9c2da54d1e76215e
Leandro-da Silva-Michail-Krikis <leandro_silva22@estudante.sesisenai.org.br>
2023-07-26 20:32:19 -0300

    Criação do model exercise

---
+++ LabMedical/src/app/shared/models/exercise.model.ts
@@ -0,0 +1,10 @@
+export interface ExerciseModel {
+  id: number
+  description: string
+  dateCreated: string
+  timeCreated: string
+  exerciseType: string
+  timesPerWeek: number
+  exerciseName: string
+  patientId: number
+}
