7c936dafb95ef428ac13f39b88d89bf40028a0dc
André Hopf <andre.hopf@gmail.com>
2023-07-27 08:59:28 -0300

    023 - adiciona método para salvar consulta

--- LabMedical/src/app/shared/services/appointments.service.ts
+++ LabMedical/src/app/shared/services/appointments.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { AuthenticationService } from "./authentication.service";
 import { Observable } from "rxjs";
 import { environment } from "../../enviroments/enviroment";
+import { Appointment } from "../models/appointment.model";
 
 @Injectable({
   providedIn: 'root'
@@ -14,10 +15,22 @@ export class AppointmentsService {
     private authenticationService: AuthenticationService
   ) { }
 
-  getAppointmentsByPacientId(pacientId: number): Observable<any> {
+  getAppointmentsByPacientId(patientId: number): Observable<any> {
     return this.http.get(
-      `${environment.URL_PATIENT_APPOINTMENTS}${pacientId}`,
+      `${environment.URL_PATIENT_APPOINTMENTS}${patientId}`,
       {headers: {"Authorization": "Bearer " + this.authenticationService.getToken()}}
+    )
+  }
+
+  saveAppointment(appointment: Appointment): Observable<any> {
+    console.log(appointment)
+
+    return this.http.post(
+      `${environment.URL_POST_APPOINTMENTS}`,
+      appointment,
+      {
+        headers: {"Authorization": "Bearer " + this.authenticationService.getToken()}
+      }
     )
   }
 }
