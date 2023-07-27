c2bca6e9c52c9cab47624534a904ab5e8aafbf3c
André Hopf <andre.hopf@gmail.com>
2023-07-27 08:58:09 -0300

    023 - adiciona estrutura inicial de formulário e listagem de consulta

--- LabMedical/src/app/components/prontuario/main-consultas/main-consultas.component.ts
+++ LabMedical/src/app/components/prontuario/main-consultas/main-consultas.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppointmentsService } from "../../../shared/services/appointments.service";
+import { Appointment } from "../../../shared/models/appointment.model";
+import { PacientService } from "../../../shared/services/pacient.service";
+import { Patient } from "../../../shared/models/patient.model";
 
 @Component({
   selector: 'app-main-consultas',
   templateUrl: './main-consultas.component.html',
   styleUrls: ['./main-consultas.component.css']
 })
-export class MainConsultasComponent {
+export class MainConsultasComponent implements OnInit {
+  showPatientAppointments: boolean = true;
+  patientId: number;
+  patientName: string;
+  patientsAppointments: Appointment[];
+
+  constructor(
+    private appointmentsService: AppointmentsService,
+    private patientService: PacientService) {
+  }
+
+  ngOnInit() {
+    this.patientId = +localStorage.getItem('patientId')
+
+    console.log(this.patientId)
+
+    if (this.patientId != 0) {
+      this.patientService.getPatient(this.patientId).subscribe({
+        next: (patient: Patient) => {
+          this.patientName = patient.name;
+        },
+        error: err => alert("Erro ao buscar paciente com o id " + this.patientId)
+      })
+
+      this.appointmentsService.getAppointmentsByPacientId(this.patientId).subscribe({
+        next: (value: Appointment[]) => {
+          this.patientsAppointments = value;
+
+          console.log(this.patientsAppointments)
+        },
+        error: err => console.log(err)
+      })
+
+    }
+  }
+
+  onAddNewAppointment() {
+    this.showPatientAppointments = !this.showPatientAppointments;
+  }
+
+  onAppointmentEdit(appointmentId: number) {
+    console.log('edit')
+  }
 
 }
---
+++ LabMedical/src/app/pages/appointment-form/appointment-form.component.ts
@@ -0,0 +1,65 @@
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Appointment } from "../../shared/models/appointment.model";
+import { NgForm } from "@angular/forms";
+import { MedicationsService } from "../../shared/services/medications.service";
+import { ModalService } from "../../shared/services/modal.service";
+import { AppointmentsService } from "../../shared/services/appointments.service";
+
+@Component({
+	selector: 'app-appointment-form',
+	templateUrl: './appointment-form.component.html',
+	styleUrls: ['./appointment-form.component.css']
+})
+export class AppointmentFormComponent implements OnInit {
+	@ViewChild('appointmentForm') appointmentForm: NgForm | undefined;
+	@Input() patientId: number;
+	MANDATORY: string = "../../../assets/images/obrigatorio.png";
+	appointmentId: number;
+	patientMedications: any[]
+	appointment: Appointment = {
+		appointmentReason: '',
+		appointmentDate: new Date().toISOString().slice(0, 10),
+		time: new Date().toLocaleTimeString('pt-BR', {timeZone: 'America/Sao_Paulo'}).slice(0, 5),
+		problemDescription: '',
+		dosageAndPrecautions: '',
+		isActive: true,
+		pacientId: null,
+		medicineId: null
+	}
+
+	constructor(
+		private medicationsService: MedicationsService,
+		private modalService: ModalService,
+		private appointmentsService: AppointmentsService) {}
+
+	ngOnInit() {
+		this.appointment.pacientId = this.patientId
+
+		this.medicationsService.getMedicationsByPatientId(this.patientId).subscribe({
+			next: medications => this.patientMedications = medications,
+			error: err => console.log(err)
+		})
+	}
+
+	onAddAppointment() {
+		console.log(this.appointment)
+
+
+		this.appointmentsService.saveAppointment(this.appointment).subscribe({
+			next: response => {
+				console.log(response)
+
+				this.modalService.createModal("Consulta cadastrada com sucesso", `Id da consulta: ${response}` )
+			},
+			error: err => console.log(err)
+		})
+	}
+
+	onSaveAppointment() {
+		console.log('editou')
+	}
+
+	onDeleteAppointment() {
+		console.log('deletou')
+	}
+}
