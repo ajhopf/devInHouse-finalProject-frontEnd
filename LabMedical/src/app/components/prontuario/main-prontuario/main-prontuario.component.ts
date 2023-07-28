e0a2ec20ad476133ad215d12582d72e0cd059cbc
André Hopf <andre.hopf@gmail.com>
2023-07-28 08:57:50 -0300

    033 - adiciona estrutura inicial à visão simplificada

--- LabMedical/src/app/components/prontuario/main-prontuario/main-prontuario.component.ts
+++ LabMedical/src/app/components/prontuario/main-prontuario/main-prontuario.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Patient } from "../../../shared/models/patient.model";
+import { PacientService } from "../../../shared/services/pacient.service";
 
 @Component({
   selector: 'app-main-prontuario',
   templateUrl: './main-prontuario.component.html',
   styleUrls: ['./main-prontuario.component.css']
 })
-export class MainProntuarioComponent {
+export class MainProntuarioComponent implements OnChanges {
+  @Input('patientId') patientId: string;
+  patient: Patient;
 
+  constructor(private patientService: PacientService) {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.patientId != undefined || this.patientId != null) {
+      this.patientService.getPatient(+this.patientId).subscribe({
+        next: (patient: Patient) => {
+          this.patient = patient;
+        },
+        error: () => alert("Erro ao buscar paciente com o id " + this.patientId)
+      })
+    }
+  }
 }
